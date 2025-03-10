f7d3e4e2390967d74ed153168c795d534968b4ff
mlivi25 <mitchlivingston25@gmail.com>
2025-03-10 10:45:28 -0500

    ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,45 @@
 import { Component } from '@angular/core';
+import { Store, createAction, createFeatureSelector, createReducer, createSelector, on, props } from '@ngrx/store';
+
+export interface CounterState {
+  count: number,
+  text: string
+}
+
+export interface AppState {
+  count : CounterState
+}
+
+
+export const incrementValue = createAction("Increment")
+export const decrementValue = createAction("Decrement")
+
+
+export const setValue = createAction("SetValue", props<{ count: number}>())
+
+export const counterReducer = createReducer(
+  { count: 0, text: "No Text"}, 
+
+  on(decrementValue, (state) => {{
+    return { ...state, count: state.count - 1}
+  }}),
+  on(incrementValue,  (state) => {
+    return {...state, count: state.count + 1}
+  }),
+  on(setValue, ( state, { count } ) => {
+    return { ...state, count: count}
+  } )
+)
+
+export const selectCounter = createFeatureSelector<CounterState>('count')
+
+
+export const selectCount = createSelector(
+  selectCounter,
+  (counterState: CounterState) => counterState.count
+)
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +48,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-practice';
+
+
+  constructor(private store: Store){}
+
+  public count$ = this.store.select(selectCount)
+
+
+  text: number = 0
+
+  increment(){
+    this.store.dispatch(incrementValue());
+  }
+
+  decrement(){
+    this.store.dispatch(decrementValue());
+  }
+
+  setValue(){
+    // this.store.dispatch(setValue({ count: this.text}))
+
+
+    this.store.select(selectCount).subscribe({
+      next: (value: any) => {console.log(value)}
+    })
+  }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent, counterReducer } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer}),
+    EffectsModule.forRoot([]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
